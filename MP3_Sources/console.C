3408cb0f267d149f45a4931609ec0057f37c7fcd
410 <guest@TA-virtualbox.(none)>
2020-09-25 13:26:37 -0500

    Making vm output to terminal for debugging purposes

--- MP3_Sources/console.C
+++ MP3_Sources/console.C
@@ -173,6 +173,9 @@ void Console::putch(const char _c){
         csr_y++;
     }
 
+    //OUTPUT TO TERMINAL
+    Machine::outportb(0xe9, _c);
+
     /* Scroll the screen if needed, and finally move the cursor */
     scroll();
     move_cursor();
