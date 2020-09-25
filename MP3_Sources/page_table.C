e0b6dd89df895ce73404dc0ca10df9321ae23cb8
410 <guest@TA-virtualbox.(none)>
2020-09-25 13:22:33 -0500

    first attempt at page table implementation

--- MP3_Sources/page_table.C
+++ MP3_Sources/page_table.C
@@ -16,27 +16,45 @@ void PageTable::init_paging(ContFramePool * _kernel_mem_pool,
                             ContFramePool * _process_mem_pool,
                             const unsigned long _shared_size)
 {
-   assert(false);
-   Console::puts("Initialized Paging System\n");
+  //Make sure pointers aren't null
+//  assert(kernel_mem_pool != NULL && process_mem_pool != NULL);
+
+  //Initialize variables
+  shared_size = _shared_size;
+  kernel_mem_pool = _kernel_mem_pool;
+  process_mem_pool = _process_mem_pool;
+
+  Console::puts("Initialized Paging System\n");
 }
 
 PageTable::PageTable()
 {
-   assert(false);
-   Console::puts("Constructed Page Table object\n");
+  //Allocate the space for the page table
+  page_directory = (unsigned long*) kernel_mem_pool->get_frames(ContFramePool::needed_info_frames(PAGE_SIZE * ENTRIES_PER_PAGE));
+
+  //Set the bits of the page entries
+  page_directory[0] |= 0x3; //set to supervisor lvl, r/w, present
+  for(int i = 1; i < 1024; i++)
+  {
+    page_directory[i] |= 0x2; //set to supervisor lvl, r/w, not present
+  }
+
+  Console::puts("Constructed Page Table object\n");
 }
 
 
 void PageTable::load()
 {
-   assert(false);
-   Console::puts("Loaded page table\n");
+  current_page_table = this;
+  Console::puts("Loaded page table\n");
 }
 
 void PageTable::enable_paging()
 {
-   assert(false);
-   Console::puts("Enabled paging\n");
+  write_cr3(current_page_table->page_directory[0]);
+  write_cr0(read_cr0() | 0x80000000);
+  paging_enabled  = 1;
+  Console::puts("Enabled paging\n");
 }
 
 void PageTable::handle_fault(REGS * _r)
